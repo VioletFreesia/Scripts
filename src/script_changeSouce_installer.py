bbd12a21029e59464c4c83d3facec0f68a40c500
violetfreesia <violetfreesia@163.com>
2020-08-30 17:15:09 +0800

    auto change system source list

---
+++ src/script_changeSouce_installer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# ^_^ coding: UTF-8 ^_^
+# -_- author: violetfreesia -_-
+
+import console
+import environment
+
+"""
+os_limit: Ubuntu
+"""
+
+
+def main():
+    if not environment.is_root():
+        return
+    console.log('---> reading system release version <---', with_header=True)
+    lsb_release = open("/etc/lsb-release")
+    lsb_release.readline()
+    lsb_release.readline()
+    version_name = lsb_release.readline().split("=")[1].replace("\n", "")
+    lsb_release.close()
+    console.info('system release version: %s' % version_name)
+    # 默认修改为阿里源
+    console.log('---> modify sources list <---', with_header=True)
+    source = '''deb http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ focal-backports main restricted universe multiverse
+    '''
+    # 修改为对应系统版本的源
+    source = source.replace("focal", version_name)
+    sources = open("/etc/apt/sources.list", "w")
+    sources.write(source)
+    sources.close()
+    console.success('---> modify success <---')
+    console.log(source)
+
+
+if __name__ == '__main__':
+    main()
